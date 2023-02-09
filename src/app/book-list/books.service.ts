6c9f24c41263ab9b78db90f8f485cb2a2afceecc
Mariyan Maksimov <mariyan.pickup@gmail.com>
2023-02-09 16:19:42 +0200

    removed unused operator

--- src/app/book-list/books.service.ts
+++ src/app/book-list/books.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-import { of, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { IBook } from './book.model';
 
@@ -9,6 +9,7 @@ import { IBook } from './book.model';
 export class GoogleBooksService {
   private readonly URL =
     'https://www.googleapis.com/books/v1/volumes?maxResults=10&orderBy=relevance&q=oliver%20sacks';
+
   constructor(private http: HttpClient) {}
 
   getBooks(): Observable<Array<IBook>> {
